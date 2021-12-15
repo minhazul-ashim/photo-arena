c969352d34b13a415063742d49deb3fcb91b4ddd
Minhazul Ashim <ashim.ashim7470@gmail.com>
2021-12-15 19:26:47 +0600

    making a carousel caption

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -3,6 +3,8 @@ import Slider from 'react-slick';
 import camera from '../../images/camera.jpg'
 import art from '../../images/art.jpg'
 import './Banner.css'
+import { Box } from '@mui/system';
+import { Grid } from '@mui/material';
 
 const Banner = () => {
 
@@ -17,14 +19,30 @@ const Banner = () => {
     return (
         <div className='carousel-parent'>
             <Slider {...settings}>
-                <div className='carousel-container'>
-                    <img className='carousel-img' src={camera} alt="" />
-                </div>
-                <div className='carousel-container'>
-                    <img className='carousel-img' src={art} alt='' />
-                </div>
+                <Grid container className='carousel-container'>
+                    <Grid item>
+                        <Box >
+                            <img className='carousel-img' src={camera} alt="" />
+                        </Box>
+                    </Grid>
+                    <Grid item>
+                        
+                    </Grid>
+                </Grid>
+
+                <Grid container className='carousel-container'>
+                    <Grid item>
+                        <Box >
+                            <img className='carousel-img' src={camera} alt="" />
+                        </Box>
+                    </Grid>
+                    <Grid item>
+                        
+                    </Grid>
+                </Grid>
+
             </Slider>
-        </div>
+        </div >
     );
 };
 
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,69 +2,82 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Tooltip from '@mui/material/Tooltip';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import Switch from '@mui/material/Switch';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormGroup from '@mui/material/FormGroup';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
 
-const Navigation = () => {
+export default function MenuAppBar() {
+    const [auth, setAuth] = React.useState(true);
+    const [anchorEl, setAnchorEl] = React.useState(null);
 
-    const [anchorElNav, setAnchorElNav] = React.useState(null);
-    const [anchorElUser, setAnchorElUser] = React.useState(null);
-
-    const handleOpenNavMenu = (event) => {
-        setAnchorElNav(event.currentTarget);
-    };
-    const handleOpenUserMenu = (event) => {
-        setAnchorElUser(event.currentTarget);
-    };
-
-    const handleCloseNavMenu = () => {
-        setAnchorElNav(null);
+    const handleChange = (event) => {
+        setAuth(event.target.checked);
     };
 
-    const handleCloseUserMenu = () => {
-        setAnchorElUser(null);
+    const handleMenu = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
     };
 
     return (
-        <AppBar position="static">
-            <Container maxWidth="xl">
-                <Toolbar disableGutters>
-
-                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                        <IconButton
-                            size="large"
-                            aria-label="account of current user"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
-                            onClick={handleOpenNavMenu}
-                            color="inherit"
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                    </Box>
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        component="div"
-                        sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
                     >
-                        PhotoArena
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        Photos
                     </Typography>
-
-                    <Box sx={{ flexGrow: 0 }}>
-                        <Tooltip>
-                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                    {auth && (
+                        <div>
+                            <IconButton
+                                size="large"
+                                aria-label="account of current user"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={handleMenu}
+                                color="inherit"
+                            >
+                                <AccountCircle />
                             </IconButton>
-                        </Tooltip>
-                    </Box>
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorEl}
+                                anchorOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                open={Boolean(anchorEl)}
+                                onClose={handleClose}
+                            >
+                                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                                <MenuItem onClick={handleClose}>My account</MenuItem>
+                            </Menu>
+                        </div>
+                    )}
                 </Toolbar>
-            </Container>
-        </AppBar>
+            </AppBar>
+        </Box>
     );
-};
-export default Navigation;
+}
